bb749324b632913f100c48954b4201810adb3d3e
Admir-Walker <aihtijarev2@etf.unsa.ba>
2021-02-14 02:40:29 +0100

    Implementacija staticke funkcije za postavljanje korisnickog tipa. Definisan vlastiti tip izuzetka kada korisnicki tip nije poznat.

--- src/app/classes/UserType.java
+++ src/app/classes/UserType.java
@@ -1,5 +1,20 @@
 package app.classes;
 
+import app.exceptions.UserTypeNotFoundException;
+
 public enum UserType {
     GOST, STUDENT, PROFESOR;
+
+    public static UserType of(int redniBroj) throws UserTypeNotFoundException {
+        switch (redniBroj){
+            case 1:
+                return UserType.GOST;
+            case 2:
+                return UserType.STUDENT;
+            case 3:
+                return UserType.PROFESOR;
+            default:
+                throw new UserTypeNotFoundException("User type not found");
+        }
+    }
 }
---
+++ src/app/exceptions/UserTypeNotFoundException.java
@@ -0,0 +1,7 @@
+package app.exceptions;
+
+public class UserTypeNotFoundException extends Exception{
+    public UserTypeNotFoundException(String message) {
+        super(message);
+    }
+}
