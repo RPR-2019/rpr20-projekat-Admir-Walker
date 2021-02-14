270359887b19c351d8f0500ef8c6bb11a5822f5c
Admir-Walker <aihtijarev2@etf.unsa.ba>
2021-02-14 03:34:38 +0100

    Implementacija klase Predmet koja modelira istoimenu tabelu iz baze.

---
+++ src/app/classes/Predmet.java
@@ -0,0 +1,62 @@
+package app.classes;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Predmet {
+    private SimpleIntegerProperty id = new SimpleIntegerProperty(0);
+    private SimpleStringProperty name = new SimpleStringProperty("");
+    private SimpleObjectProperty<User> profesor = new SimpleObjectProperty<>(null);
+
+
+    public Predmet(int id, String name, User profesor) {
+        this.id.set(id);
+        this.name.set(name);
+        this.profesor.set(profesor);
+    }
+
+    public Predmet() {
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public User getProfesor() {
+        return profesor.get();
+    }
+
+    public SimpleObjectProperty<User> profesorProperty() {
+        return profesor;
+    }
+
+    public void setProfesor(User profesor) {
+        this.profesor.set(profesor);
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
