fca8e4940c5fe30f8529d782d1f248a094403612
Admir-Walker <aihtijarev2@etf.unsa.ba>
2021-02-15 02:11:10 +0100

    Implementirane klase za upravljanje dokumentima

---
+++ src/app/classes/Document.java
@@ -0,0 +1,126 @@
+package app.classes;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.time.LocalDate;
+
+public class Document {
+    private SimpleIntegerProperty id = new SimpleIntegerProperty(0);
+    private SimpleStringProperty name = new SimpleStringProperty("");
+    private SimpleStringProperty path = new SimpleStringProperty("");
+    private SimpleStringProperty uploadDate = new SimpleStringProperty(LocalDate.now().toString());
+    private SimpleObjectProperty<Subject> subject = new SimpleObjectProperty<>(null);
+    private SimpleObjectProperty<User> author = new SimpleObjectProperty<>(null);
+    private SimpleBooleanProperty downloadable = new SimpleBooleanProperty(false);
+    private SimpleIntegerProperty size = new SimpleIntegerProperty(0);
+
+    public Document(int id, String name, String path, String uploadDate, Subject subject, User author, boolean downloadable, int size) {
+        this.id.set(id);
+        this.name.set(name);
+        this.path.set(path);
+        this.uploadDate.set(uploadDate);
+        this.subject.set(subject);
+        this.author.set(author);
+        this.downloadable.set(downloadable);
+        this.size.set(size);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getPath() {
+        return path.get();
+    }
+
+    public SimpleStringProperty pathProperty() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path.set(path);
+    }
+
+    public String getUploadDate() {
+        return uploadDate.get();
+    }
+
+    public SimpleStringProperty uploadDateProperty() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(String uploadDate) {
+        this.uploadDate.set(uploadDate);
+    }
+
+    public Subject getSubject() {
+        return subject.get();
+    }
+
+    public SimpleObjectProperty<Subject> subjectProperty() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject.set(subject);
+    }
+
+    public User getAuthor() {
+        return author.get();
+    }
+
+    public SimpleObjectProperty<User> authorProperty() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author.set(author);
+    }
+
+    public boolean isDownloadable() {
+        return downloadable.get();
+    }
+
+    public SimpleBooleanProperty downloadableProperty() {
+        return downloadable;
+    }
+
+    public void setDownloadable(boolean downloadable) {
+        this.downloadable.set(downloadable);
+    }
+
+    public int getSize() {
+        return size.get();
+    }
+
+    public SimpleIntegerProperty sizeProperty() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size.set(size);
+    }
+}
---
+++ src/app/models/DocumentDAO.java
@@ -0,0 +1,62 @@
+package app.models;
+
+import app.classes.Document;
+import app.classes.Subject;
+import app.classes.User;
+import app.settings.Database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DocumentDAO {
+    private static DocumentDAO instance;
+    private static Database database;
+
+    private PreparedStatement fetchDocuments;
+
+    private DocumentDAO() throws SQLException {
+        database = Database.getInstance();
+        fetchDocuments = database.addPreparedStatement(
+                "select * from document d\n" +
+                "inner join user u on d.author = u.userID\n" +
+                "where subject = ?;"
+        );
+    }
+
+    public static DocumentDAO getInstance() throws SQLException {
+        if (instance == null) instance = new DocumentDAO();
+        return instance;
+    }
+
+    public static void removeInstance() throws SQLException {
+        if (instance == null) return;
+        Database.removeInstance();
+        instance = null;
+    }
+    public List<Document> fetchDocumentList(Subject subject){
+        try {
+            fetchDocuments.setInt(1, subject.getId());
+            ResultSet resultSet = fetchDocuments.executeQuery();
+            List<Document> documentList = new ArrayList<>();
+            while (resultSet.next()){
+                int documentID = resultSet.getInt(1);
+                String documentName = resultSet.getString(2);
+                String documentPath = resultSet.getString(3);
+                String documentUploadDate = resultSet.getString(4);
+                boolean documentIsDownloadable = resultSet.getInt(7) == 1;
+                int size = resultSet.getInt(8);
+                int userID = resultSet.getInt(9);
+                User author = UserDAO.getInstance().fetchUserViaID(userID);
+                documentList.add(new Document(documentID, documentName, documentPath, documentUploadDate, subject, author, documentIsDownloadable, size));
+            }
+            return documentList;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+}
+
