50315705ada9fc8e378868077b77a50d98b2c8d1
Admir-Walker <aihtijarev2@etf.unsa.ba>
2021-02-15 02:13:01 +0100

    Promjena sa bosanskog na engleski

--- src/app/classes/UserType.java
+++ src/app/classes/UserType.java
@@ -3,13 +3,13 @@ package app.classes;
 import app.exceptions.UserTypeNotFoundException;
 
 public enum UserType {
-    GUEST, STUDENT, PROFESOR;
+    GUEST, STUDENT, PROFESSOR;
 
     public static UserType of(int redniBroj) throws UserTypeNotFoundException {
         return switch (redniBroj) {
             case 1 -> UserType.GUEST;
             case 2 -> UserType.STUDENT;
-            case 3 -> UserType.PROFESOR;
+            case 3 -> UserType.PROFESSOR;
             default -> throw new UserTypeNotFoundException("User type not found");
         };
     }
